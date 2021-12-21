c0a7eb7e87611579abee829e5693c4672935437c
Sergey Konash <sergeikonash@gmail.com>
2021-12-21 22:58:59 +0300

    HW2. Third commit

--- src/home_work_2/SortsUtils.java
+++ src/home_work_2/SortsUtils.java
@@ -1,8 +1,24 @@
 package home_work_2;
 
+import java.util.Arrays;
+
 public class SortsUtils {
     public static void main(String[] args) {
-
+        int[] arr = {2, 5, 3, 1, 2, 8, 7};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+        int[] arr3_1 = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(arr3_1));
+        System.out.println(Arrays.toString(bubbleSort(arr3_1)));
+        int[] arr3_2 = {1, 1, 1, 1};
+        System.out.println(Arrays.toString(arr3_2));
+        System.out.println(Arrays.toString(bubbleSort(arr3_2)));
+        int[] arr3_3 = {9, 1, 5, 99, 9, 9};
+        System.out.println(Arrays.toString(arr3_3));
+        System.out.println(Arrays.toString(bubbleSort(arr3_3)));
+        int[] arr3_4 = {};
+        System.out.println(Arrays.toString(arr3_4));
+        System.out.println(Arrays.toString(bubbleSort(arr3_4)));
     }
     public static int[] bubbleSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
